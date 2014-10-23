29a9f002ee7fbf67e7ddede8727d753f369f1a21
4lk4tr43 <4lk4tr43@gmail.com>
2014-10-23 15:25:44 +0200

    Update

---
+++ ProceduralBuildings/Program.fs
@@ -0,0 +1,39 @@
+﻿module Program
+
+open System.Windows.Forms
+open System.Drawing
+
+open Screen
+
+let pen = new Pen(Color.Green, 5.0f)
+
+let random = System.Random(0)
+let number_of_parameters_per_rect = 3
+let number_of_rects = 20
+
+let sparse = 500.0
+let biggest_rect = 800.0
+let rectangles : seq<Rectangle> = seq { 
+    for i in 1.0..double(number_of_rects) ->
+        new Rectangle(
+            int(random.NextDouble() * sparse), 
+            int(random.NextDouble() * sparse),
+            int(biggest_rect/i*random.NextDouble()),
+            int(biggest_rect/i*random.NextDouble()) 
+    )}
+
+let form = new PaintForm(1500.0, 1000.0)    
+form.Text <- "Canvas"
+form.StartPosition <- FormStartPosition.CenterScreen
+form.Paint.Add(fun context ->
+    for i in rectangles do
+        context.Graphics.DrawRectangle(pen, i)
+    )
+    
+let timer = new System.Timers.Timer(3000.0)
+timer.Elapsed.Add(fun e -> 
+    form.Invalidate()
+    )
+timer.Start()
+
+Application.Run(form)
---
+++ ProceduralBuildings/Screen.fs
@@ -0,0 +1,32 @@
+﻿module Screen
+
+open System.Drawing
+
+type Canvas(width:double, height:double) =
+        
+    let Size = width, height
+
+    member this.ToAbsolute x y = System.Math.Round(x * fst Size), System.Math.Round(y * snd Size)
+    member this.ToRelative x y = double(x) / fst Size, double(y) / snd Size
+
+    member this.ToAbsolutes a = a |> Array.map(fun e -> int(fst e * fst Size), int(snd e * snd Size))
+    member this.ToRelatives a = a |> Array.map(fun e -> fst e / fst Size, snd e / snd Size)
+
+
+open System.Windows.Forms
+
+type PaintForm(width:double, height:double) as this =
+    inherit Form() 
+
+    let initialize = 
+        this.ClientSize <- new System.Drawing.Size(int(width), int(height))
+        this.Load.Add(fun e -> this.ImproveDisplay)
+
+    member this.Canvas = new Canvas(width, height)
+
+    member this.ImproveDisplay =         
+        this.SetStyle(System.Windows.Forms.ControlStyles.UserPaint ||| 
+                      System.Windows.Forms.ControlStyles.AllPaintingInWmPaint ||| 
+                      System.Windows.Forms.ControlStyles.OptimizedDoubleBuffer, 
+                      true)
+        this.DoubleBuffered <- true
